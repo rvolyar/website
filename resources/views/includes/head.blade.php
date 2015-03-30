862429cfb1d9ed8844b45b2c30415ba7ad3f5c4b
Abhinav Madahar <veenamadahar@gmail.com>
2015-03-30 22:00:52 +0000

    Removed junk
    The default.blade.php file used to be the homepage, and I just changed some lines to make it into the default layout.
    However, this meant that there was some customizations left over that made it act like the homepage, like having the index.html's CSS built-in.
    
    I removed the customizations, as well as added some commentation and directions for how to use the layout.

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -6,8 +6,9 @@
 
 -->
 
-<title>Charles T. King</title>
+<!--jQuery-->
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+<!--Bootstrap-->
 <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 <link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"/>
-<link rel="stylesheet" type="text/css" href="index.css"/>
