9f23f355ae507b27511a56c953781ebd0ebd587f
Abhinav Madahar <AbhinavMadahar@users.noreply.github.com>
2015-03-30 17:21:45 -0400

    This script updates a fork
    If you fork this repo, then running this script will merge the commits from the original repo into your fork. I will test if this works later.
    
    By the way, this is a bash script, so the syntax may look weird.

---
+++ fork-update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# THIS FILE UPDATES FORKS TO THE LATEST VERSION OF THE ORIGINAL REPOSITORY
+# USE THIS COMMAND TO RUN THIS FILE:
+# ./fork-update.sh develop
+
+if [ -n "$1" ]; then # if the first argument does not exist
+	$branch = "develop";
+else
+	$branch = $1;
+fi
+
+echo "Updating $branch branch";
+
+# set up the variables
+$owner = "Charles-T-King";
+$repo = "website";
+
+git checkout $branch
+git pull https://github.com/$owner/$repo.git $branch
+
+echo "Please commit the changes if you are satisfied"
