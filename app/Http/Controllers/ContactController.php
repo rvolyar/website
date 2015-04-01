77d9df13eb42e4de82f61196f1add9ac94597f9c
Asena Yildiz <16yildiza@millburn.org>
2015-04-01 18:10:45 +0000

    changed contact controller, made contact view

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -29,7 +29,7 @@ class ContactController extends Controller {
 	 */
 	public function index()
 	{
-		return "contact page";
+		return view('contact');
 	}
 
 }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.default')
 
 @section('content')
-
+    <h1>WAAAAAZZZ UPPP CONTACT USS DAUG</h1>
 @stop
