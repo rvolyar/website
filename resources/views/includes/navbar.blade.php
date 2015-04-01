6da5a91b42f6b7f91fc8c32c90167f6f0772aea3
Abhinav Madahar <veenamadahar@gmail.com>
2015-04-01 20:16:54 +0000

    Fixed the navigation bar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -7,7 +7,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#"><img alt="Brand" id="nav-logo" src="resources/logo.png"/></a>
+			<a class="navbar-brand" href="#"><img alt="Brand" id="nav-logo" src="images/favicon.png"/></a>
 		</div>
 
 		<!-- Collect the nav links, forms, and other content for toggling -->
