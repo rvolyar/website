9992cb6451620fc450d990b8c79578917cdc7eae
Asena Yildiz <16yildiza@millburn.org>
2015-04-02 18:06:56 +0000

    commits

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>WAAAAAZZZ UPPP CONTACT USS DAUG</h1>
+        <div class="jumbotron">
+            <div class="container-fluid">
+                <h1>Contact Us</h1>
+                <p>Feel free to contact us at any time with any questions you may have.</p>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <h1>Contact Information</h1>
+            <h4>General Inquiries:</h4>
+            <a href="mailto:charlestking.info@gmail.com">charlestking.info@gmail.com</a></br>
+            <h4>Current & Past Loan Recipients:</h4>
+            <a href="mailto:charlestking.treasurer@gmail.com">charlestking.treasurer@gmail.com</a></br>
+            <h4>Mail to:</h4><br>
+            Charles T. King
+        </div>
 @stop
