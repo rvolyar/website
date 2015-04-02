082d6124a185888c0725bf4ba333b0dd2efd62a0
Asena Yildiz <16yildiza@millburn.org>
2015-04-02 18:28:09 +0000

    Finish contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,15 +7,17 @@
                 <p>Feel free to contact us at any time with any questions you may have.</p>
             </div>
         </div>
-        <div class="container-fluid">
-            <h1>Contact Information</h1><br>
-            <h4>General Inquiries:</h4>
+        <div class="col-md-5">
+            <h2>General Inquiries:</h2>
             <a href="mailto:charlestking.info@gmail.com">charlestking.info@gmail.com</a></br>
-            <h4>Current & Past Loan Recipients:</h4>
+            <h2>Current & Past Loan Recipients:</h2>
             <a href="mailto:charlestking.treasurer@gmail.com">charlestking.treasurer@gmail.com</a></br>
-            <h4>Mail to:</h4>
+            <h2>Mail to:</h2>
             <p>Charles T. King Student Loan Fund<br>
             P.O. Box 272<br>
             Short Hills, NJ 07078<br></p>
+        </div>
+        <div class="col-md-7">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d1511.9174297271727!2d-74.31204086244627!3d40.72165175657065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1smillburn+education+center!5e0!3m2!1sen!2s!4v1427999114660" width="600" height="450" frameborder="0" style="border:0"></iframe>
         </div>
 @stop
