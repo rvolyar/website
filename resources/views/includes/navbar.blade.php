59c02fee82bfc72a4b252007fff00879fe918344
Ruslan Volyar <prizerebelcrap101@gmail.com>
2015-04-03 18:28:15 +0000

    cleaned up navbar, deleted unused features in first release

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -17,39 +17,6 @@
 				</li>
 				<li><a href="application.html">Apply for a Loan</a>
 				</li>
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Student Profile<span class="caret"></span></a>
-					<ul class="dropdown-menu" role="menu">
-						<li><a href="student/profile.html">Full Student Profile</a></li>
-						<li class="divider"></li>
-						<li><a href="student/payments.html#remaining">Payments Remaining</a></li>
-						<li><a href="student/payments.html#paid">Paid Payments</a></li>
-						<li class="divider"></li>
-						<li><a href="student/contact.html">Personal Contact Information</a>
-						</li>
-					</ul>
-				</li>
-			</ul>
-			<form class="navbar-form navbar-left" role="search">
-				<div class="form-group">
-					<input type="text" class="form-control" placeholder="Search the FAQ">
-				</div>
-				<button type="submit" class="btn btn-default">Search</button>
-			</form>
-			<ul class="nav navbar-nav navbar-right">
-				<li><a href="admin.html">Admin</a>
-				</li>
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Help<span class="caret"></span></a>
-					<ul class="dropdown-menu" role="menu">
-						<li><a href="faq.html">Frequently Asked Questions</a></li>
-						<li class="divider"></li>
-
-						<li><a href="forum.html">Help Forum</a></li>
-						<li><a href="mailto:charlestking.info@gmail.com">Email Us</a>
-						</li>
-					</ul>
-				</li>
 			</ul>
 		</div><!-- /.navbar-collapse -->
 	</div><!-- /.container-fluid -->
