60cec9cb49f744ae1e00174149f8da33c0aba9aa
Ruslan Volyar <prizerebelcrap101@gmail.com>
2015-04-12 20:53:33 +0000

    simplified bash update command

--- update.sh
+++ update.sh
@@ -2,27 +2,27 @@
 
 # THIS FILE UPDATES FORKS TO THE LATEST VERSION OF THE ORIGINAL REPOSITORY
 # USE THIS COMMAND TO RUN THIS FILE:
-# bash fork-update.sh develop
+# bash update.sh
 
-if [ "$1" == " " ]; then # if the first argument does not exist
-	branch="develop";
-else
-	branch=$1;
-fi
+# if [ "$1" == " " ]; then # if the first argument does not exist
+# #	branch="develop";
+#else
+#	branch=$1;
+#fi
 
-echo "Updating $branch branch";
+echo "Updating local develop branch";
 
 # set up the variables
 owner="Charles-T-King";
 repository="website";
 
 # avoid losing your work if there are conflicts
-git add *; # add all the files
-git stash; # avoid losing your work
+#git add *; # add all the files
+#git stash; # avoid losing your work
 
 git remote add upstream git://github.com/$owner/$repository.git
 git remote set-url upstream git://github.com/$owner/$repository.git
 git fetch upstream
-git rebase upstream/$branch
+git rebase upstream/develop
 
 echo "Your fork is now synced with the upstream fork."
