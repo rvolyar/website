af94ecda4c53b8ac6458506e6d53ac03c13ba334
Isabella Waung <izzy.waung@gmail.com>
2015-04-13 18:06:07 +0000

    Created about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default')
+
+@section('content')
+    <h1>About the Charles T. King Student Loan Fund</h1>
+    
+    <div>
+        <p>The Charles T. King Student Loan fund provides interest-free loans to Millburn Township residents who wish to pursue a course sof study in any college, university, specialized career training school, or post-graduate program (as approved by the Student Loan Fund Board) and who might otherwise be financially unable to realize their goals.
+        </p>
+    </div>
+    <div>
+       <p>
+           The Fund began in 1931 and was renamed in 1974 to honor Dr. King, a former superintendent of schools. Since its inception, over 675 township students have been assisted by the Fund.
+       </p>
+    </div>
+@stop
+
